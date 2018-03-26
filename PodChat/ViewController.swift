f0a9cfc8929c1accb24d23976174eef6c2e2cde4
Michael J. Miller <mikem.exe@gmail.com>
2018-03-26 11:36:18 -0600

    Initial Commit

---
+++ PodChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PodChat
+//
+//  Created by Michael Miller on 3/26/18.
+//  Copyright © 2018 Puffin Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
